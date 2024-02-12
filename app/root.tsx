c778208f247bf063cea6c753fcf5fa34f27d0dd2
Quang Dat Le <quangdatlecz@gmail.com>
2024-02-12 01:13:46 +0100

    add sass package

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,40 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction } from "@netlify/remix-runtime"
+import { cssBundleHref } from "@remix-run/css-bundle"
+import type { MetaFunction } from "@remix-run/node"
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from "@remix-run/react"
 
-export const meta: MetaFunction = () => [{
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-}];
+export const meta: MetaFunction = () => [
+	{
+		charset: "utf-8",
+		title: "New Remix App",
+		viewport: "width=device-width,initial-scale=1",
+	},
+]
+
+export const links: LinksFunction = () => [
+	...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+]
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	)
 }
