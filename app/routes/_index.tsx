c4ee412ace2a9468d0fa42170f519dd51d0b6637
Quang Dat Le <quangdatlecz@gmail.com>
2024-02-12 01:27:13 +0100

    Add supabase - initial
    @TODO: query from ParticipationSubmission table

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,26 +1,36 @@
+import { useLoaderData } from "@remix-run/react"
+import { supabase } from "utils/supabase"
 import { Hero } from "~/components/Hero"
 
 export function headers({
-  loaderHeaders,
-  parentHeaders,
+	loaderHeaders,
+	parentHeaders,
 }: {
 	loaderHeaders: Headers
 	parentHeaders: Headers
 }) {
-  console.log(
-    "This is an example of how to set caching headers for a route, feel free to change the value of 60 seconds or remove the header"
+	console.log(
+		"This is an example of how to set caching headers for a route, feel free to change the value of 60 seconds or remove the header"
 	)
-  return {
-    // This is an example of how to set caching headers for a route
-    // For more info on headers in Remix, see: https://remix.run/docs/en/v1/route/headers
-    "Cache-Control": "public, max-age=60, s-maxage=60",
+	return {
+		// This is an example of how to set caching headers for a route
+		// For more info on headers in Remix, see: https://remix.run/docs/en/v1/route/headers
+		"Cache-Control": "public, max-age=60, s-maxage=60",
 	}
 }
 
+export const loader = async () => {
+	const { data } = await supabase.from("test").select()
+
+	return { data }
+}
+
 export default function Index() {
-  return (
-    <main style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+	const { data } = useLoaderData<typeof loader>()
+	return (
+		<main style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+			<pre>{JSON.stringify(data, null, 2)}</pre>
 			<Hero />
-    </main>
+		</main>
 	)
 }
