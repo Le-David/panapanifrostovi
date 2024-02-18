bbe7dcb48b323595e36a7d2b376cb96b3b0b2cb4
Quang Dat Le <quangdatlecz@gmail.com>
2024-02-18 02:46:27 +0100

    query the data from ParticipationSubmission table and render - initial
    @TODO: format data

--- app/routes/very-secret-page.tsx
+++ app/routes/very-secret-page.tsx
@@ -7,12 +7,35 @@ export const loader = async () => {
 }
 
 export default function VerySecretPage() {
-	const data = useLoaderData<typeof loader>()
+	const { data } = useLoaderData<typeof loader>()
 	// @TODO: schema
 
 	return (
 		<main>
-			<pre>{JSON.stringify(data, null, 2)}</pre>
+			{data && data.length > 0 && (
+				<ul>
+					{data.map((submission) => (
+						// @TODO: format data
+						<li key={submission.id}>
+							<ul>
+								<li>Datum odeslání: {submission.created_at}</li>
+								<li>
+									Jméno a přijmení: {submission.firstname} {submission.lastname}
+								</li>
+								<li>Telefon: {submission.mobile}</li>
+								<li>
+									Zúčastní se: {submission.is_participating ? "ano" : "ne"}
+								</li>
+								<li>
+									Jedná se o rodinu: {submission.is_family ? "ano" : "ne"} (
+									{submission.participants})
+								</li>
+								<li>Poznámka: {submission.note}</li>
+							</ul>
+						</li>
+					))}
+				</ul>
+			)}
 		</main>
 	)
 }
