da6b5c4eeeaf313fd3f6166ecf0c4c03215f31e2
Quang Dat Le <quangdatlecz@gmail.com>
2024-02-27 01:28:29 +0100

    Adjust hero - initial

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,4 +1,5 @@
 import type { FunctionComponent } from "react"
+import { Image } from "~/components/Image"
 import styles from "./Hero.module.css"
 
 export type HeroProps = {}
@@ -6,7 +7,31 @@ export type HeroProps = {}
 export const Hero: FunctionComponent<HeroProps> = ({}) => {
 	return (
 		<div className={styles.wrapper}>
-			<h1 className={styles.title}>Web budoucího pana a paní Frostovi</h1>
+			<div className={styles.is_desktop}>
+				<div className={styles.initial}>K</div>
+				<Image
+					isGoogleDrive
+					imageId="1sDjdEE6ZG1_MC5_SVcEYBijguncgf0Tz"
+					width={480}
+					height={675}
+					alt="Pan a paní Frostovi"
+				/>
+				<div className={styles.initial}>D</div>
+			</div>
+			<div className={styles.is_mobile}>
+				<Image
+					isGoogleDrive
+					imageId="1sDjdEE6ZG1_MC5_SVcEYBijguncgf0Tz"
+					width={480}
+					height={675}
+					alt="Pan a paní Frostovi"
+				/>
+				<div className={styles.content}>
+					<div className={styles.initial}>K</div>
+					<div className={styles.ampersand}>&</div>
+					<div className={styles.initial}>D</div>
+				</div>
+			</div>
 		</div>
 	)
 }
