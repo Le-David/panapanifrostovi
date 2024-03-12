4b33c2e99c62689d22cab3f5d3425b3735522a29
Quang Dat Le <quangdatlecz@gmail.com>
2024-03-13 00:01:56 +0100

    Add font "Barlow"

--- app/root.tsx
+++ app/root.tsx
@@ -35,6 +35,16 @@ export default function App() {
 	return (
 		<html lang="en">
 			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="anonymous"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,500;0,700;1,500;1,700&display=swap"
+					rel="stylesheet"
+				/>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link
 					rel="apple-touch-icon"
