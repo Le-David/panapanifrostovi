244611a10c32e43c3c348a37de14b0f23c77edf7
Quang Dat Le <quangdatlecz@gmail.com>
2024-03-12 21:34:02 +0100

    Adjust Hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,33 +5,15 @@ import styles from "./Hero.module.css"
 export const Hero: FunctionComponent = () => {
 	return (
 		<div className={styles.wrapper}>
-			<div className={styles.is_desktop}>
-				<div className={styles.initial}>K</div>
-				<Image
-					className={styles.image}
-					isGoogleDrive
-					imageId="1sDjdEE6ZG1_MC5_SVcEYBijguncgf0Tz"
-					width={480}
-					height={675}
-					alt="Pan a paní Frostovi"
-				/>
-				<div className={styles.initial}>D</div>
-			</div>
-			<div className={styles.is_mobile}>
-				<Image
-					className={styles.image}
-					isGoogleDrive
-					imageId="1sDjdEE6ZG1_MC5_SVcEYBijguncgf0Tz"
-					width={480}
-					height={675}
-					alt="Pan a paní Frostovi"
-				/>
-				<div className={styles.content}>
-					<div className={styles.initial}>K</div>
-					<div className={styles.ampersand}>&</div>
-					<div className={styles.initial}>D</div>
-				</div>
-			</div>
+			<Image
+				className={styles.image}
+				isGoogleDrive
+				imageId="1sDjdEE6ZG1_MC5_SVcEYBijguncgf0Tz"
+				width={480}
+				height={675}
+				alt="Pan a paní Frostovi"
+			/>
+			<h1 className={styles.title}>Vítejte na našem svatebním webu</h1>
 		</div>
 	)
 }
