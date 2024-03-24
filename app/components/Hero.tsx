33158cb48448eb604784fc3fa52053812e8a82e4
Quang Dat Le <quangdatlecz@gmail.com>
2024-03-24 18:44:23 +0100

    Adjust Hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,17 +6,23 @@ import styles from "./Hero.module.css"
 export const Hero: FunctionComponent = () => {
 	return (
 		<div className={styles.wrapper}>
-			<Image
-				className={styles.image}
-				isGoogleDrive
-				imageId="1sDjdEE6ZG1_MC5_SVcEYBijguncgf0Tz"
-				width={480}
-				height={675}
-				alt="Pan a paní Frostovi"
-			/>
-			<Container>
-				<h1 className={styles.title}>Vítejte na našem svatebním webu</h1>
-			</Container>
+			<div className={styles.background}>
+				<Image
+					className={styles.image}
+					isGoogleDrive
+					imageId="1c0nGIAMB6UT3kc-xVULwTkVSPDk9Yx4N"
+					width={1800}
+					height={810}
+					fill
+					alt="Pan a paní Frostovi"
+				/>
+			</div>
+			<div className={styles.content}>
+				<Container>
+					<h1 className={styles.title}>Vítejte na našem svatebním webu</h1>
+					<h2>#novotnajemrazena</h2>
+				</Container>
+			</div>
 		</div>
 	)
 }
