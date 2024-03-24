6fd248af05e7b786949f3d5f7d4e3b5e3d692bc4
Quang Dat Le <quangdatlecz@gmail.com>
2024-03-24 20:16:31 +0100

    Adjust structure and styles for very secret page

--- app/routes/very-secret-page.tsx
+++ app/routes/very-secret-page.tsx
@@ -1,5 +1,8 @@
 import { useLoaderData } from "@remix-run/react"
+import clsx from "clsx"
+import { useCallback } from "react"
 import { supabase } from "utils/supabase"
+import styles from "../styles/routes/very-secret-page.module.css"
 
 export const loader = async () => {
 	const { data } = await supabase.from("ParticipationSubmission").select()
@@ -8,33 +11,49 @@ export const loader = async () => {
 
 export default function VerySecretPage() {
 	const { data } = useLoaderData<typeof loader>()
+	const handleDate = useCallback((date: string) => {
+		return new Date(date).toLocaleDateString("cs-CZ")
+	}, [])
 	// @TODO: schema
 
 	return (
-		<main>
+		<main className={styles.wrapper}>
+			<h2>Tabulka</h2>
 			{data && data.length > 0 && (
-				<ul>
-					{data.map((submission) => (
-						// @TODO: format data
-						<li key={submission.id}>
-							<ul>
-								<li>Datum odeslání: {submission.created_at}</li>
-								<li>
-									Jméno a přijmení: {submission.firstname} {submission.lastname}
-								</li>
-								<li>Telefon: {submission.mobile}</li>
-								<li>
-									Zúčastní se: {submission.is_participating ? "ano" : "ne"}
-								</li>
-								<li>
-									Jedná se o rodinu: {submission.is_family ? "ano" : "ne"} (
-									{submission.participants})
-								</li>
-								<li>Poznámka: {submission.note}</li>
-							</ul>
-						</li>
-					))}
-				</ul>
+				<table className={styles.table}>
+					<thead>
+						<tr>
+							<th className={clsx(styles.cell, styles.is_heading)}>
+								Datum odeslání
+							</th>
+							<th className={clsx(styles.cell, styles.is_heading)}>
+								Jméno a přijmení
+							</th>
+							<th className={clsx(styles.cell, styles.is_heading)}>Telefon</th>
+							<th className={clsx(styles.cell, styles.is_heading)}>
+								Zúčastní se
+							</th>
+							<th className={clsx(styles.cell, styles.is_heading)}>Poznámka</th>
+						</tr>
+					</thead>
+					<tbody>
+						{data.map((submission) => (
+							<tr key={submission.id}>
+								<td className={clsx(styles.cell)}>
+									{handleDate(submission.created_at)}
+								</td>
+								<td className={clsx(styles.cell)}>
+									{submission.firstname} {submission.lastname}
+								</td>
+								<td className={clsx(styles.cell)}>{submission.mobile}</td>
+								<td className={clsx(styles.cell)}>
+									{submission.is_participating ? "ano" : "ne"}
+								</td>
+								<td className={clsx(styles.cell)}>{submission.note}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
 			)}
 		</main>
 	)
