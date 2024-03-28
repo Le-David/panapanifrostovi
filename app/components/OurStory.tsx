37ca615dd8c4bc878f17aff52398bc83f901a80f
Quang Dat Le <quangdatlecz@gmail.com>
2024-03-29 00:14:44 +0100

    OurStory: add fade in effects

--- app/components/OurStory.tsx
+++ app/components/OurStory.tsx
@@ -1,4 +1,6 @@
-import type { FunctionComponent } from "react"
+import clsx from "clsx"
+import { type FunctionComponent } from "react"
+import { useInView } from "react-intersection-observer"
 import { Container } from "./Container"
 import { Image } from "./Image"
 import styles from "./OurStory.module.css"
@@ -12,42 +14,52 @@ export const OurStory: FunctionComponent = () => {
 						<h2 className={styles.title}>Kristýna a David</h2>
 					</div>
 					<div className={styles.images}>
-						<div className={styles.image}>
-							<Image
-								className={styles.imageIn}
-								isGoogleDrive
-								imageId="177fYXDKNBmTOjqPnAtLTX8CesNvnTlOY"
-								width={300}
-								height={533}
-								alt="Naše zasnoubení"
-								fill
-							/>
-						</div>
-						<div className={styles.image}>
-							<Image
-								className={styles.imageIn}
-								isGoogleDrive
-								imageId="1QHvHh4wi6K6NsKTrWkxsldynE6XMafJT"
-								width={300}
-								height={400}
-								alt="Naše zasnoubení"
-								fill
-							/>
-						</div>
-						<div className={styles.image}>
-							<Image
-								className={styles.imageIn}
-								isGoogleDrive
-								imageId="1Shcmx3ouCM96dwfsSiibZZFBUpRQg72A"
-								width={300}
-								height={400}
-								alt="Naše zasnoubení"
-								fill
-							/>
-						</div>
+						<ImageComponent
+							imageId="177fYXDKNBmTOjqPnAtLTX8CesNvnTlOY"
+							width={300}
+							height={533}
+							alt="Mladíci"
+						/>
+						<ImageComponent
+							imageId="1QHvHh4wi6K6NsKTrWkxsldynE6XMafJT"
+							width={300}
+							height={400}
+							alt="My"
+						/>
+						<ImageComponent
+							imageId="1Shcmx3ouCM96dwfsSiibZZFBUpRQg72A"
+							width={300}
+							height={400}
+							alt="Naše zasnoubení"
+						/>
 					</div>
 				</div>
 			</Container>
+		</div>
+	)
+}
+
+const ImageComponent: FunctionComponent<{
+	imageId: string
+	width: number
+	height: number
+	alt: string
+}> = ({ imageId, width, height, alt }) => {
+	const { ref, inView } = useInView({
+		triggerOnce: true,
+		threshold: 0.5,
+	})
+	return (
+		<div className={clsx(styles.image, inView && styles.is_inView)} ref={ref}>
+			<Image
+				className={styles.imageIn}
+				isGoogleDrive
+				imageId={imageId}
+				width={width}
+				height={height}
+				alt={alt}
+				fill
+			/>
 		</div>
 	)
 }
